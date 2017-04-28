1586e1a4f6e74b5c497579438879b725fdfcdd10
RevenueWire <swang@revenuewire.com>
2017-04-28 16:49:30 +0000

    pipe: workspace

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="pipe: test propel and skd generation"
+    release_note="pipe: workspace"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
