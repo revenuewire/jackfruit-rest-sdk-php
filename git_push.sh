fdd12ddd7f9a18f985ddaac75707f531fb279e97
RevenueWire <swang@revenuewire.com>
2017-04-28 21:26:33 +0000

    pipe: try again

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="pipe: fix the states"
+    release_note="pipe: try again"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
