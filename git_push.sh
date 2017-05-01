0d11c6a7ed4c36ce60b0f4642009b577277875f6
RevenueWire <swang@revenuewire.com>
2017-05-01 20:44:54 +0000

    Automatic merge from release/1-staging -> development
    * commit '602ba6f2eab437142e07fee12073cf19ffe7c7e0':
      Merge pull request #621 in SMP/jackfruit from feature/SMP-8697-banner to release/0-blue

--- git_push.sh
+++ git_push.sh
@@ -20,8 +20,8 @@ fi
 if [ "$release_note" = "" ]; then
     release_note="Automatic merge from release/1-staging -> development
 
-* commit '0cc71731fe877ffdf0edb7b2ad7589cb366659ef':
-  Merge pull request #620 in SMP/jackfruit from feature/SMP-8735-RR to release/0-blue"
+* commit '602ba6f2eab437142e07fee12073cf19ffe7c7e0':
+  Merge pull request #621 in SMP/jackfruit from feature/SMP-8697-banner to release/0-blue"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
