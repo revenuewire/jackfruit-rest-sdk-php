0a46dbb45280af702c9bf292beab0ad967d731bd
RevenueWire <swang@revenuewire.com>
2017-05-02 21:57:25 +0000

    Automatic merge from release/1-staging -> development
    * commit '9b215e4e601f5b6a516f0d7bb3cc9f7947220db7':
      Merge pull request #629 in SMP/jackfruit from feature/SMP-8779 to release/1-staging

--- git_push.sh
+++ git_push.sh
@@ -18,39 +18,10 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Merge pull request #630 in SMP/jackfruit from feature/SMP-8552-dev to development
+    release_note="Automatic merge from release/1-staging -> development
 
-Squashed commit of the following:
-
-commit 502006b3c8c03038c344396a241087662548911d
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Tue May 2 14:35:18 2017 -0700
-
-    SMP-8552 last bill date
-
-commit d0505b372ad71baaa2eb3091b3c57e4c08e6bcf7
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Tue May 2 14:19:55 2017 -0700
-
-    SMP-8552 clean up
-
-commit 17b60da730df088cc39d0f0b9b3d42ab2d1804e0
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Tue May 2 14:18:09 2017 -0700
-
-    SMP-8552 last billing date
-
-commit 507be7fbb8434a29341abe32aa1bf767491441ed
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Fri Apr 28 10:03:24 2017 -0700
-
-    SMP-8552 clean up
-
-commit be517cc6cf2881ba787dfbd6130016a244a77c46
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Fri Apr 21 16:24:33 2017 -0700
-
-    phones fix"
+* commit '9b215e4e601f5b6a516f0d7bb3cc9f7947220db7':
+  Merge pull request #629 in SMP/jackfruit from feature/SMP-8779 to release/1-staging"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
