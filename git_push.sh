aaf9ac7282c456e95b497664c19ca222c8ef6b73
RevenueWire <swang@revenuewire.com>
2017-05-09 19:23:00 +0000

    Merge pull request #581 in SMP/jackfruit from feature/smp-8677-click-id-fix to development
    Squashed commit of the following:
    
    commit 86e53fd205aba0b157bd132cbac4bbac228aaa88
    Author: stanley <sli@revenuewire.com>
    Date:   Tue May 2 15:04:25 2017 -0700
    
        SMP-8677 Click ID Fix
    
    commit ac9d82c8cc32e1d6d3fee2e0aab02ce80529c037
    Merge: c778e57 9f9a8a0
    Author: stanley <sli@revenuewire.com>
    Date:   Tue May 2 13:24:35 2017 -0700
    
        Merge branch 'development' into feature/smp-8677-click-id-fix
    
    commit c778e577984cba4f8a9f1c57c68cc9d53c894742
    Author: stanley <sli@revenuewire.com>
    Date:   Thu Apr 27 16:04:08 2017 -0700
    
        SMP-8677 Click Id Fix
    
    commit 83b8801768b5f8815fae73be4221a58481d9fe4a
    Merge: 4232ae3 644b027
    Author: stanley <sli@revenuewire.com>
    Date:   Thu Apr 27 15:28:10 2017 -0700
    
        Merge branch 'development' into feature/smp-8677-click-id-fix
    
    commit 4232ae322ccc7e8b0bca05b83d80fbdea9c42730
    Author: stanley <sli@revenuewire.com>
    Date:   Wed Apr 26 15:58:36 2017 -0700
    
        SMP-8677 Click Id Fix
    
    commit 6d18b181c68f7d8cfb619f61560be4e92df78d5d
    Merge: 40beb45 f837821
    Author: stanley <sli@revenuewire.com>
    Date:   Wed Apr 26 13:27:10 2017 -0700
    
        Merge branch 'development' into feature/smp-8677-click-id-fix
    
    commit 40beb452729556f3d561b4944236292f54384da1
    Author: stanley <sli@revenuewire.com>
    Date:   Mon Apr 24 13:25:09 2017 -0700
    
        SMP-8677 Click Id
    
    commit cc5014a98d983efac9789d2b569f1e4c8904ec0f
    Merge: dd968c7 3629824
    Author: stanley <sli@revenuewire.com>
    Date:   Mon Apr 24 13:24:47 2017 -0700
    
        Merge branch 'development' into feature/smp-8677-click-id-fix
    
    commit dd968c7b7232681c358c7546baf88522376ed831
    Merge: e79af50 3fb3290
    Author: stanley <sli@revenuewire.com>
    Date:   Sun Apr 23 11:04:07 2017 -0700
    
        Merge branch 'development' into feature/smp-8677-click-id-fix
    
    commit e79af505e50ba96735cfebc1694aa10ac12b2d9d
    Author: stanley <sli@revenuewire.com>
    Date:   Thu Apr 13 15:27:59 2017 -0700
    
        SMP-8677 event notification added
    
    commit 7a127f3421dfc78ee31262877f076e8ac29b84a2
    Author: stanley <sli@revenuewire.com>
    Date:   Thu Apr 13 11:04:20 2017 -0700
    
        SMP-8677 Click Id Fix

--- git_push.sh
+++ git_push.sh
@@ -18,69 +18,80 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Merge pull request #650 in SMP/jackfruit from feature/SMP-8552-dev to development
+    release_note="Merge pull request #581 in SMP/jackfruit from feature/smp-8677-click-id-fix to development
 
 Squashed commit of the following:
 
-commit 86470407a48e13d21472ca84dd3b147b6826a8f3
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Mon May 8 10:31:05 2017 -0700
+commit 86e53fd205aba0b157bd132cbac4bbac228aaa88
+Author: stanley <sli@revenuewire.com>
+Date:   Tue May 2 15:04:25 2017 -0700
 
-    SMP-8552 document lookup API
+    SMP-8677 Click ID Fix
 
-commit 7cacd725253da42255220bd4d821f093964fc92f
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Thu May 4 14:03:14 2017 -0700
+commit ac9d82c8cc32e1d6d3fee2e0aab02ce80529c037
+Merge: c778e57 9f9a8a0
+Author: stanley <sli@revenuewire.com>
+Date:   Tue May 2 13:24:35 2017 -0700
 
-    SMP-8552 clean up
+    Merge branch 'development' into feature/smp-8677-click-id-fix
 
-commit b61ad9779f602534a1673015327ed9567dcaeabf
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Thu May 4 14:00:09 2017 -0700
+commit c778e577984cba4f8a9f1c57c68cc9d53c894742
+Author: stanley <sli@revenuewire.com>
+Date:   Thu Apr 27 16:04:08 2017 -0700
 
-    SMP-8552 adding account phone to users api
+    SMP-8677 Click Id Fix
 
-commit 850c906e85838a73e79ab67cd775d0cb0a3f539e
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Tue May 2 15:15:04 2017 -0700
+commit 83b8801768b5f8815fae73be4221a58481d9fe4a
+Merge: 4232ae3 644b027
+Author: stanley <sli@revenuewire.com>
+Date:   Thu Apr 27 15:28:10 2017 -0700
 
-    SMP-8552 final date fix
+    Merge branch 'development' into feature/smp-8677-click-id-fix
 
-commit 74b9ff55e8990e9178cfbcfa58e9e61f2f0b9f23
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Tue May 2 15:06:39 2017 -0700
+commit 4232ae322ccc7e8b0bca05b83d80fbdea9c42730
+Author: stanley <sli@revenuewire.com>
+Date:   Wed Apr 26 15:58:36 2017 -0700
 
-    SMP-8552 last date fix
+    SMP-8677 Click Id Fix
 
-commit 7895f39f93846eda1fc1fa6a31adb31faafb2a97
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Tue May 2 14:35:18 2017 -0700
+commit 6d18b181c68f7d8cfb619f61560be4e92df78d5d
+Merge: 40beb45 f837821
+Author: stanley <sli@revenuewire.com>
+Date:   Wed Apr 26 13:27:10 2017 -0700
 
-    SMP-8552 last bill date
+    Merge branch 'development' into feature/smp-8677-click-id-fix
 
-commit 949c9ea6a4427eab94c45cf99e8748f0309bf828
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Tue May 2 14:19:55 2017 -0700
+commit 40beb452729556f3d561b4944236292f54384da1
+Author: stanley <sli@revenuewire.com>
+Date:   Mon Apr 24 13:25:09 2017 -0700
 
-    SMP-8552 clean up
+    SMP-8677 Click Id
 
-commit 26974f8f25c9b2b908c8418f463637df47d090fd
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Tue May 2 14:18:09 2017 -0700
+commit cc5014a98d983efac9789d2b569f1e4c8904ec0f
+Merge: dd968c7 3629824
+Author: stanley <sli@revenuewire.com>
+Date:   Mon Apr 24 13:24:47 2017 -0700
 
-    SMP-8552 last billing date
+    Merge branch 'development' into feature/smp-8677-click-id-fix
 
-commit 596d8829ea82c34e49482d5682b3f24c8d661ad7
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Fri Apr 28 10:03:24 2017 -0700
+commit dd968c7b7232681c358c7546baf88522376ed831
+Merge: e79af50 3fb3290
+Author: stanley <sli@revenuewire.com>
+Date:   Sun Apr 23 11:04:07 2017 -0700
 
-    SMP-8552 clean up
+    Merge branch 'development' into feature/smp-8677-click-id-fix
 
-commit a5d3dfa5a7d3fff943242d4defe7cc11d0bb76d2
-Author: Guy Segal <gsegal@revenuewire.com>
-Date:   Fri Apr 21 16:24:33 2017 -0700
+commit e79af505e50ba96735cfebc1694aa10ac12b2d9d
+Author: stanley <sli@revenuewire.com>
+Date:   Thu Apr 13 15:27:59 2017 -0700
 
-    phones fix"
+    SMP-8677 event notification added
+
+commit 7a127f3421dfc78ee31262877f076e8ac29b84a2
+Author: stanley <sli@revenuewire.com>
+Date:   Thu Apr 13 11:04:20 2017 -0700
+
+    SMP-8677 Click Id Fix"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
