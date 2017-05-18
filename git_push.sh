2d834e1440d2d3036d4b43e1c7e2ec2b2bbd10af
RevenueWire <swang@revenuewire.com>
2017-05-18 23:27:35 +0000

    Merge pull request #716 in SMP/jackfruit from feature/smp-8823-coupon-fix to development
    Squashed commit of the following:
    
    commit ceac18fa50461d4a321645ecc41a6c89a6f04d5c
    Author: stanley <sli@revenuewire.com>
    Date:   Thu May 18 10:26:46 2017 -0700
    
        SMP-8823 Coupon Fix

--- git_push.sh
+++ git_push.sh
@@ -18,41 +18,15 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Merge pull request #649 in SMP/jackfruit from feature/SMP-8794 to development
+    release_note="Merge pull request #716 in SMP/jackfruit from feature/smp-8823-coupon-fix to development
 
 Squashed commit of the following:
 
-commit 39a901d9859a9e3fc56e38e3ec1bbf06af8e85af
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Thu May 18 13:27:01 2017 -0700
+commit ceac18fa50461d4a321645ecc41a6c89a6f04d5c
+Author: stanley <sli@revenuewire.com>
+Date:   Thu May 18 10:26:46 2017 -0700
 
-    SMP-8794 update user emaill and pdate according to code reveiw.
-
-commit d0babcf35e63a61180b72000754cc4cf60143b1b
-Merge: 318e8b9 cfbc65e
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Thu May 18 13:12:11 2017 -0700
-
-    Merge branch 'development' into feature/SMP-8794
-
-commit 318e8b931c379294f93c6da7cb08f04736394ce7
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Mon May 8 16:28:01 2017 -0700
-
-    SMP-8794 Update customer email api, and update according to code review.
-
-commit 743f169b58fbdbbeaf6d2724889c056d0385a319
-Merge: f9d9208 fcbab88
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Mon May 8 16:21:52 2017 -0700
-
-    Merge branch 'development' into feature/SMP-8794
-
-commit f9d92083ad56b9530055bad0588fbcd2ec34218f
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Fri May 5 11:17:17 2017 -0700
-
-    SMP-8794 Add the ability to edit user email in the users API"
+    SMP-8823 Coupon Fix"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
