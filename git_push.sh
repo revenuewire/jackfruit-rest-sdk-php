53fd30153a7c131d0621f4da151564c307bdb8cf
RevenueWire <swang@revenuewire.com>
2017-05-25 23:39:50 +0000

    Merge pull request #728 in SMP/jackfruit from feature/smp-8602-cpa-credit-limit to development
    Squashed commit of the following:
    
    commit b768ac7036515837097ca1781fe39a13c063b9bf
    Author: stanley <sli@revenuewire.com>
    Date:   Thu May 25 10:53:52 2017 -0700
    
        SMP-8602 CPA Credit Limit Code Review Fix
    
    commit ba09679fa37d817b93f833a6b6f517ff50a28ca7
    Merge: c17d7af 5ab5c58
    Author: stanley <sli@revenuewire.com>
    Date:   Thu May 25 10:30:54 2017 -0700
    
        Merge branch 'development' into feature/smp-8602-cpa-credit-limit
    
    commit c17d7af6af5e6970efc507478dd902c1f39792eb
    Author: stanley <sli@revenuewire.com>
    Date:   Tue May 23 15:15:35 2017 -0700
    
        SMP-8602 CPA Credit Limit
    
    commit b6e60319d88b85652106cf75dc14bc60b1502d2e
    Merge: e06332d d5671ff
    Author: stanley <sli@revenuewire.com>
    Date:   Tue May 23 15:12:13 2017 -0700
    
        Merge branch 'development' into feature/smp-8602-cpa-credit-limit
    
    commit e06332d0d4fa556b4d25e30dd4727e469f110b79
    Author: stanley <sli@revenuewire.com>
    Date:   Tue May 23 15:10:49 2017 -0700
    
        SMP-8602 CPA Credit Limit
    
    commit 20d5a31b78595006baa1f9c8e0225936d27d8f9d
    Author: stanley <sli@revenuewire.com>
    Date:   Tue May 23 14:43:42 2017 -0700
    
        SMP-8602 CPA Credit Limit
    
    commit bd9794d223c8c2d01538242af4190f62763828eb
    Author: stanley <sli@revenuewire.com>
    Date:   Tue May 23 14:27:43 2017 -0700
    
        SMP-8602 CPA Credit Limit

--- git_push.sh
+++ git_push.sh
@@ -18,15 +18,53 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Merge pull request #743 in SMP/jackfruit from feature/SMP-8112 to development
+    release_note="Merge pull request #728 in SMP/jackfruit from feature/smp-8602-cpa-credit-limit to development
 
 Squashed commit of the following:
 
-commit d344ec3576ae35f823fe9d6ac1131bea73395f07
-Author: Scott Wang <swang@ceastgaard.revenuewire.com>
-Date:   Thu May 25 14:36:32 2017 -0700
+commit b768ac7036515837097ca1781fe39a13c063b9bf
+Author: stanley <sli@revenuewire.com>
+Date:   Thu May 25 10:53:52 2017 -0700
 
-    SMP-8112: set max expiry year to 15 years"
+    SMP-8602 CPA Credit Limit Code Review Fix
+
+commit ba09679fa37d817b93f833a6b6f517ff50a28ca7
+Merge: c17d7af 5ab5c58
+Author: stanley <sli@revenuewire.com>
+Date:   Thu May 25 10:30:54 2017 -0700
+
+    Merge branch 'development' into feature/smp-8602-cpa-credit-limit
+
+commit c17d7af6af5e6970efc507478dd902c1f39792eb
+Author: stanley <sli@revenuewire.com>
+Date:   Tue May 23 15:15:35 2017 -0700
+
+    SMP-8602 CPA Credit Limit
+
+commit b6e60319d88b85652106cf75dc14bc60b1502d2e
+Merge: e06332d d5671ff
+Author: stanley <sli@revenuewire.com>
+Date:   Tue May 23 15:12:13 2017 -0700
+
+    Merge branch 'development' into feature/smp-8602-cpa-credit-limit
+
+commit e06332d0d4fa556b4d25e30dd4727e469f110b79
+Author: stanley <sli@revenuewire.com>
+Date:   Tue May 23 15:10:49 2017 -0700
+
+    SMP-8602 CPA Credit Limit
+
+commit 20d5a31b78595006baa1f9c8e0225936d27d8f9d
+Author: stanley <sli@revenuewire.com>
+Date:   Tue May 23 14:43:42 2017 -0700
+
+    SMP-8602 CPA Credit Limit
+
+commit bd9794d223c8c2d01538242af4190f62763828eb
+Author: stanley <sli@revenuewire.com>
+Date:   Tue May 23 14:27:43 2017 -0700
+
+    SMP-8602 CPA Credit Limit"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
