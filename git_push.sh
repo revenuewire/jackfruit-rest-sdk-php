dfa12b93592aee25029912fedd18a59ae9778c79
RevenueWire <swang@revenuewire.com>
2017-05-26 23:19:50 +0000

    Pipe: test deploy

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Pipeline: more smaller fixes"
+    release_note="Pipe: test deploy"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
