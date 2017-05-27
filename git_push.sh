df4d85e0d0464a76a70a4207a959dca1824d6fce
RevenueWire <swang@revenuewire.com>
2017-05-27 01:54:40 +0000

    Pipe: fix cache dir

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Pipe: fix rancher"
+    release_note="Pipe: fix cache dir"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
