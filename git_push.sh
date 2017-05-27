3726b211e2383c503a7640c1a980c61fc3b43e37
RevenueWire <swang@revenuewire.com>
2017-05-27 00:26:42 +0000

    Pipe: fix rancher

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Pipe: test deploy"
+    release_note="Pipe: fix rancher"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
