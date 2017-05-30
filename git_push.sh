a1ac6d55e094648ac607749663e8b6f53304ef49
RevenueWire <swang@revenuewire.com>
2017-05-30 21:39:07 +0000

    SMP-8861: fix redis

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="SMP-8861: fix runtime conf"
+    release_note="SMP-8861: fix redis"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
