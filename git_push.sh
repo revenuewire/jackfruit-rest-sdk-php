8d0e2cd0df0aac75c3fbf92105f235762dfe18b3
RevenueWire <swang@revenuewire.com>
2017-06-14 03:40:22 +0000

    Merge pull request #801 in SMP/jackfruit from feature/SMP-8685 to development
    SMP-8685 worldpay renewals failing because of new non-recurring purchase by the same customer
    
    Squashed commit of the following:
    
    commit 5f5f60649dd858601d941fd7adacd465681ba7c0
    Merge: 1c3392b 659696b
    Author: Jeff Richards <jrichards@revenuewire.com>
    Date:   Tue Jun 13 12:37:37 2017 -0700
    
        Merge branch 'development' of http://git.rw.app:7990/scm/smp/jackfruit into feature/SMP-8685
    
    commit 1c3392b70283bc2838f5991ac2122fe5fc11eea4
    Author: Jeff Richards <jrichards@revenuewire.com>
    Date:   Thu Jun 8 12:16:03 2017 -0700
    
        SMP-8685 adjust PagBrasil to avoid the same mistake that the WorldPayProcessor had
    
    commit e4f4f6eff4801a99146f70baf86b28928c1050d4
    Author: Jeff Richards <jrichards@revenuewire.com>
    Date:   Wed Jun 7 13:15:20 2017 -0700
    
        SMP-8685 worldpay renewals failing because of new non-recurring purchase by the same customer

--- git_push.sh
+++ git_push.sh
@@ -18,73 +18,30 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Merge pull request #805 in SMP/jackfruit from feature/SMP-8867 to development
+    release_note="Merge pull request #801 in SMP/jackfruit from feature/SMP-8685 to development
+
+SMP-8685 worldpay renewals failing because of new non-recurring purchase by the same customer
 
 Squashed commit of the following:
 
-commit 17978d7c50181c92bfc6cbff56fd97b45d98279d
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Mon Jun 12 11:05:59 2017 -0700
+commit 5f5f60649dd858601d941fd7adacd465681ba7c0
+Merge: 1c3392b 659696b
+Author: Jeff Richards <jrichards@revenuewire.com>
+Date:   Tue Jun 13 12:37:37 2017 -0700
 
-    SMP-8867 SPO Paypal payment update. Update according to code review.
+    Merge branch 'development' of http://git.rw.app:7990/scm/smp/jackfruit into feature/SMP-8685
 
-commit 0d5f93c81870b33a9429cff4cc030e71034e41a9
-Merge: 935342e aa358b7
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Mon Jun 12 09:42:57 2017 -0700
+commit 1c3392b70283bc2838f5991ac2122fe5fc11eea4
+Author: Jeff Richards <jrichards@revenuewire.com>
+Date:   Thu Jun 8 12:16:03 2017 -0700
 
-    Merge branch 'development' into feature/SMP-8867
+    SMP-8685 adjust PagBrasil to avoid the same mistake that the WorldPayProcessor had
 
-commit 935342ec153f24c40c060e182a8834f40e2ccdb3
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Fri Jun 9 16:00:23 2017 -0700
+commit e4f4f6eff4801a99146f70baf86b28928c1050d4
+Author: Jeff Richards <jrichards@revenuewire.com>
+Date:   Wed Jun 7 13:15:20 2017 -0700
 
-    SMP-8867 SPO Paypal payment update API
-
-commit 04aa0b522db9b44c7a702ac70b65ec1bdc6cce57
-Merge: 3dcbe52 f1e07ca
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Fri Jun 9 15:43:31 2017 -0700
-
-    Merge branch 'development' into feature/SMP-8867
-
-commit 3dcbe52118f4cad3b7c98a0f74c70d0d09e8dfd6
-Merge: 1a73dec 92d2c77
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Fri Jun 9 12:17:25 2017 -0700
-
-    Merge branch 'development' into feature/SMP-8867
-
-commit 1a73decbabad56a4e77e4a6c50214a7a7767a1df
-Merge: f6fdb0b d9a913f
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Fri Jun 9 09:13:54 2017 -0700
-
-    Merge branch 'development' into feature/SMP-8867
-
-commit f6fdb0b5eee69de3e896e3f452d09d0b66bb2cbc
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Fri Jun 9 09:13:11 2017 -0700
-
-    SMP-8867 SPO Paypal payment update API. Update according to code review.
-
-commit 37345f5e1fe090912feeef165c6556b9bbe1e6e1
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Thu Jun 8 16:17:56 2017 -0700
-
-    SMP-8867 Paypal Update payment API. update according to code review.
-
-commit 30a82115d7dd44c541160ab4c83d55f089aaf43e
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Thu Jun 8 12:23:54 2017 -0700
-
-    SMP-8867 SPO Paypal payment update API. update according to code reveiw
-
-commit eb4025e94122d4e7a1554f0b5c5575fdc6f6a52c
-Author: Leo Shen <lshen@lshen2.revenuewire.com>
-Date:   Thu Jun 1 16:12:18 2017 -0700
-
-    SMP-8867 SPO Paypal payment update API"
+    SMP-8685 worldpay renewals failing because of new non-recurring purchase by the same customer"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
