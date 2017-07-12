f00f01cb8bc5c95d0b03a80cb7d1cefccea469b7
RevenueWire <swang@revenuewire.com>
2017-07-12 20:14:20 +0000

    rancher: try again before upgrade jenkins

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="rancher: try again"
+    release_note="rancher: try again before upgrade jenkins"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
