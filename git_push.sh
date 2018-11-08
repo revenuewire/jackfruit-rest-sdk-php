8c3adfbbbd32e6418b3275ca24ce48e84732c843
RevenueWire <swang@revenuewire.com>
2018-11-08 20:34:04 +0000

    1402

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="1401"
+    release_note="1402"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
